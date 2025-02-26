c0a86d4ca00a18c5a273ebcabd459f33633fdbd6
yolaatar <laatarjo@gmail.com>
2025-02-26 10:51:12 +0100

    changement symbole

--- symbole.cpp
+++ symbole.cpp
@@ -1,5 +1,6 @@
 #include "symbole.h"
 #include <iostream>
+#include <map>
 
 void Symbole::Affiche() {
    cout<<Etiquettes[ident];
@@ -10,3 +11,9 @@ void Entier::Affiche() {
    cout<<"("<<valeur<<")";
 }
 
+class Expr : public Symbole {
+   public:
+      Expr(int i) : Symbole(i) { }
+      virtual ~Expr() { }
+      virtual double eval(const map<string,double> & valeurs) const = 0;
+};
